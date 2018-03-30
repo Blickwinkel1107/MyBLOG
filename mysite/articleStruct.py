9bf3f61457c83d1c28e63ab3856b45740101a936
Blickwinkel <1196061706@qq.com>
2018-03-30 18:17:15 +0800

    博文删除和侧边栏优化 增加博文删除功能，每个博文对应一个侧边栏评论 还未开放评论功能

---
+++ mysite/articleStruct.py
@@ -0,0 +1,12 @@
+class Art:
+    def __init__(self, time, content, comment):
+        self.time = time
+        self.content = content
+        self.comment = comment
+
+class Comm:
+    def __init__(self, time, content, link, author):
+        self.time = time
+        self.content = content
+        self.link = link
+        self.author = author
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.db import connection
 from django.views.decorators.csrf import csrf_exempt
-from pymysql import converters
+from mysite.articleStruct import *
 
 #index
 def index(req):
@@ -16,12 +16,29 @@ def articles(req):
     ctx = {}
     c = connection.cursor()
     c.execute('select * from articles order by time desc')
-    ctx['articles'] = c.fetchall()
+    Arts = []
+    vec_art = c.fetchall()
+    for it_art in vec_art:
+        siderName = 'sider-art-%s' % it_art[0]
+        c.execute('select * from `%s` order by time desc', siderName)
+        vec_comm = c.fetchall()
+        Comms = []
+        for it_comm in vec_comm:
+            Comms.append(Comm(it_comm[0], it_comm[1], it_comm[2], it_comm[3]))
+        Arts.append(Art(it_art[0], it_art[1], Comms))
+    ctx['Arts'] = Arts
     if req.is_ajax():
         # print(req.body)
-        mdHTML = req.POST['mdHTML']
-        timestamp = req.POST['timestamp']
-        c.execute('insert into articles(time, content) values(%s, %s)', [timestamp, mdHTML])
+        if req.POST.get('crateArt'):
+            mdHTML = req.POST['mdHTML']
+            timestamp = req.POST['timestamp']
+            c.execute('insert into articles(time, content) values(%s, %s)', (timestamp, mdHTML))
+            siderName = 'sider-art-%s' % timestamp
+            c.execute('CREATE TABLE `%s` (  `time` VARCHAR(30) NOT NULL,  `content` VARCHAR(100) NOT NULL,  `link` VARCHAR(30) NOT NULL,  `author` VARCHAR(30) NOT NULL,  PRIMARY KEY (`time`))', siderName)
+        if req.POST.get('delArt'):
+            c.execute('DELETE FROM `articles` WHERE `time`=%s', req.POST['time'])
+            siderName = 'sider-art-%s' % req.POST['time']
+            c.execute('DROP TABLE `%s`', siderName)
     return render(req, 'articles.html', ctx)
 
 def hello(req):
